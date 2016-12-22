cb378dd9906939f0f6575ca7e27c2ec179e77afe
Marta Costa <martapcosta@gmail.com>
2016-12-22 18:09:36 +0100

    avec le dtd et la classe ValidateXmlWithDtd

---
+++ src/main/java/ch/marta/xml/ValidateXmlWithDtd.java
@@ -0,0 +1,57 @@
+package ch.marta.xml;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.XMLReader;
+
+public class ValidateXmlWithDtd {
+	public static void validate() throws Exception {
+		InputSource source = new InputSource("document.xml");
+
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		factory.setValidating(true);
+		factory.setNamespaceAware(true);
+
+		SAXParser parser = factory.newSAXParser();
+		XMLReader reader = parser.getXMLReader();
+
+		final int[] errors = new int[] { 0, 0 };
+		reader.setErrorHandler(new ErrorHandler() {
+			@Override
+			public void fatalError(SAXParseException exception)
+					throws SAXException {
+				errors[0]++;
+				System.out.printf("Line: %d%nFatal Error: %s %n",
+						exception.getLineNumber(), exception.getMessage());
+			}
+
+			@Override
+			public void error(SAXParseException e) throws SAXParseException {
+				errors[0]++;
+				System.out.printf("Line: %d%nError: %s%n", e.getLineNumber(),
+						e.getMessage());
+			}
+
+			@Override
+			public void warning(SAXParseException err)
+					throws SAXParseException {
+				errors[1]++;
+				System.out.printf("Line: %d%nWarning: %s%n",
+						err.getLineNumber(), "Warning: " + err.getMessage());
+			}
+		});
+		reader.parse(source);
+
+		if (errors[0] == 0) {
+			System.out.println("Le document est valide.");
+		} else {
+			System.out.printf("Le document contient %d erreurs.%n", errors[0]);
+			System.exit(1);
+		}
+	}
+}
---
+++ styles.xsl
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+<xsl:attribute-set name="CustomStyles">
+    <xsl:attribute name="border">1pt solid black</xsl:attribute>
+    <xsl:attribute name="text-align">center</xsl:attribute>
+</xsl:attribute-set>
+</xsl:stylesheet>
